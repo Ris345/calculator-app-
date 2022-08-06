8619fd6ab8f2c8afa346b54fac5d6b73282ae4cd
Dorjee <rishav345@gmail.com>
2022-08-06 12:58:38 -0400

    update

--- cd.js
+++ cd.js
@@ -1,5 +1,4 @@
 
-
 const display = document.getElementById('display'); // main display
 const main = document.getElementById('main'); // main div 
 let number = document.getElementsByClassName('number'); // numbers 
@@ -14,7 +13,7 @@ let equals = document.getElementById('equals') // equals button
 let results = 0
 let  firstOperand =  " "
 let  secondOperand =  " "
-let operator = " "; 
+let operator = ""; // the akward space in the operator creates problems 
 let zeroDisplay = 0
 
 
@@ -24,39 +23,29 @@ Array.from(number).forEach(num => {
         //firstOperand = parseFloat(e.target.value)
         if (!operator) {
             firstOperand = parseFloat(e.target.value)
+            //console.log(firstOperand)
+            display.innerText += firstOperand
             console.log(firstOperand)
-            display.innerText = firstOperand
-
         } else {
             secondOperand = parseFloat(e.target.value)
             console.log(secondOperand)
-            display.innerText = parseFloat(e.target.value)
+            display.innerText += parseFloat(e.target.value)
 
         }
-
     })
 });
-
 
 // grabbing each operator with onClick.
 Array.from(Operator).forEach(opr  => {
     opr.addEventListener('click', function (e) {
+        operator = e.target.value
         
     })
 });
 
 
 // grabbing decimal and equals with onClick. 
-Array.from(oprCollection).forEach(spec => {
-    spec.addEventListener('click', function (e) {
-        if (spec = "=") {
-            //equal()
-        } else if (spec = ".") {
-            console.log('this is a decimal point')
-        }
-       
-    })
-})
+
 
 
 
@@ -64,8 +53,25 @@ equals.addEventListener("click", function (d) {
     console.log('firstOperand',firstOperand)
     console.log('secondOperand', secondOperand)
     console.log('operator', operator)
-    if (operator == "=") {
-        let result = firstOperand + secondOperand
-        console.log(result)
-    }
+    console.log(equals.value)
+    if (equals.value = "=") {
+        if (operator = "+") {
+            results = firstOperand + secondOperand
+            console.log(results)
+            
+        } 
+        
+        //let result = firstOperand + secondOperand
+        //calculate()
+        //display.innerText = result
+        //console.log(result)
+    } 
 })
+
+//function calculate() {
+    //let result = (operator == "+" ) ? firstOperand + secondOperand :
+    //(operator == "-") ? firstOperand - secondOperand :
+    //(operator == "x") ? firstOperand * secondOperand :
+                //firstOperand / secondOperand;
+    
+//}
