3c735d0f6c7eeea455ba29fa11292ab5cc814147
Dorjee <rishav345@gmail.com>
2022-08-14 17:23:52 -0400

    real commit

--- cd.js
+++ cd.js
@@ -1,28 +1,24 @@
 
-let display = document.getElementById('display'); // main display
-const main = document.getElementById('main'); // main div 
-let number = document.getElementsByClassName('number'); // numbers 
-//let  operators = document.getElementsByClassName('operator'); // 2nd div
-const button = document.getElementById('operators'); // buttons for operator 
-const box = document.getElementById('box'); // number div 
-let firstOpr = document.getElementById('firstOpr') // 1st display
-let secondOpr = document.getElementById('secondOpr') // 2nd display 
-let operate = document.getElementById('operate')
-let Operator = document.getElementsByClassName('operator') // operator 
-let clearButton = document.getElementById('clearButton') // . & = 
-let equals = document.getElementById('equals') // equals button 
-let results = 0
-let  firstOperand =  " "
-let  secondOperand =  " "
+const display = document.getElementById('display'); 
+const main = document.getElementById('main');  
+let number = document.getElementsByClassName('number');  
+const button = document.getElementById('operators');  
+const box = document.getElementById('box');  
+let firstOpr = document.getElementById('firstOpr'); // 1st display
+let secondOpr = document.getElementById('secondOpr'); // 2nd display 
+let operate = document.getElementById('operate');
+let Operator = document.getElementsByClassName('operator'); // operator 
+let clearButton = document.getElementById('clearButton'); // . & = 
+let equals = document.getElementById('equals'); // equals button 
+let results = 0;
+let firstOperand = " ";
+let secondOperand = " ";
 let operator = ""; 
 
 
-//  grabbing each number with onClick.
 Array.from(number).forEach(num => {
     num.addEventListener('click', function (e) {
-        //firstOperand = parseFloat(e.target.value) 
-        if (operator == "") { // !operator previous logic 
-            //firstOperand = parseFloat(e.target.value) 
+        if (operator == "") { 
             if (display.innerText.length >= 10) return;
             if (firstOperand) {
                 firstOperand += e.target.value            
@@ -30,39 +26,73 @@ Array.from(number).forEach(num => {
                 firstOperand = parseFloat(e.target.value)                
              }
                 
-            display.innerText = firstOperand            
+            display.innerText = firstOperand   
            
-     } else  {  
+        } else if (firstOperand && operator) {  
             if (display.innerText.length >= 10) return;
             if (secondOperand) {
                 secondOperand += e.target.value   
             } else {
                 secondOperand = parseFloat(e.target.value)
-                
             }
             display.innerText = secondOperand 
-           
-        } 
-        
+            
+        }  
     })
 });
 
 Array.from(Operator).forEach(opr  => {
     opr.addEventListener('click', function (e) {
-        operator = e.target.value 
         if (display.innerText.length >= 10) {
             display.innerText = "";
+            
         }
-         // if operator = + 
-         // then perform operation
-         // store result of operation in secondOperand 
-        if (operator =="+") {
-            results = parseFloat(firstOperand) + parseFloat(secondOperand)
-            debugger; 
-            results = secondOperand
-
+        if (operator == "+") {
+            if (secondOperand) {
+                results = parseFloat(firstOperand) + parseFloat(secondOperand)
+                display.innerText = results
+                
+                firstOperand = results
+                secondOperand = " "
+            } else {
+                display.innerText = firstOperand
+            }
         }
-
+        if (operator == "-") {
+            if (secondOperand) {
+                results = parseFloat(firstOperand) - parseFloat(secondOperand)
+                display.innerText = results
+               
+                firstOperand = results 
+                secondOperand = " "
+            } else {
+                display.innerText = firstOperand
+            }
+        }
+        if (operator == "*") {
+            if (secondOperand) {
+                results = parseFloat(firstOperand) * parseFloat(secondOperand)
+                display.innerText = results 
+               
+                firstOperand = results
+                secondOperand = " "
+            } else {
+                display.innerText = firstOperand
+            }
+        }
+        if (operator == "/") {
+            if (secondOperand) {
+                results = parseFloat(firstOperand) / parseFloat(secondOperand)
+                display.innerText = results
+               
+                firstOperand = results 
+                secondOperand = " "
+            } else {
+                display.innerText = firstOperand
+            }
+        }
+        operator = e.target.value 
+          operate.innerText = operator
     })
 });
 
@@ -74,102 +104,49 @@ decimal.addEventListener("click", function () {
      
 })
 
-
-// grabbing decimal and equals with onClick. 
+ 
 equals.addEventListener("click", function () {
-    console.log('firstOperand',firstOperand)
-    console.log('secondOperand', secondOperand)
-    console.log('operator', operator)
-    console.log(equals.value)
-    debugger;
-    if (equals.value == "=") {
-        debugger;            
-    }
+        calculate()         
+})
+
+
+function calculate() {
     if (operator == "+") {
         results = parseFloat(firstOperand) + parseFloat(secondOperand) 
         display.innerText = results
-        add()
+     
+
       } else if (operator == "-") {
         results = parseFloat(firstOperand) - parseFloat(secondOperand)
         display.innerText = results
-        subtract()
+     
     } else if (operator == "*") {
         results = parseFloat(firstOperand) * parseFloat(secondOperand)
         display.innerText = results
-        multiply()
-    } else {
-        results = parseFloat(firstOperand) / parseFloat(secondOperand)
-        display.innerText = (results).toFixed(3)
-        divide()
+       
+
+    } else if (operator == "/") {
+        results = parseFloat(firstOperand) / parseFloat(secondOperand) 
+        display.innerText = results.toFixed(3)  
+       
     } 
-           
-})
-
-function add() {
-     parseFloat(firstOperand) + parseFloat(secondOperand)
-     
 }
 
-function subtract() {
-     parseFloat(firstOperand) - parseFloat(secondOperand)
-     
-}
 
-function multiply() {
-    parseFloat(firstOperand) * parseFloat(secondOperand)
-}
 
-function divide() {
-    parseFloat(firstOperand) / parseFloat(secondOperand)
-}
-
- 
-clearButton.addEventListener("click", function () {
+clear.addEventListener("click", function () {
     display.innerText = 0;
     if (!firstOperand == "" || !operator == "" || !secondOperand == "") {
       firstOperand = "";
       operator = "";
       secondOperand = "";
+        
     }
 })
 
 
 
 
-// 1 + 1 + 1 + 1 = 4 
-// continued calculation 
-// press one, 1 should show up 
-// firstopr = 1 
-// secondOpr = ""
-// operator = ""
-//press + , nothing should show up 
-// firstOpr = 1
-// secondOpr = ""
-// operator = "+"
-// press one, 1 should show up 
-//firstOpr = 1 
-// secondOpr = ""
-// operator = "+"
-// press + , 2 should show up 
-// firstOpr = 2
-// secondOpr = ""
-// operator = "+"
-// press 1 , one should show up 
-// firstOpr = 2
-// secondOpr = ""
-// operator = "+"
-// press + , 3 should show up 
-// firstOpr = 3 
-// second = ""
-// operator "="
-// press one , 1 should show up 
-// firstOpr = 3
-// secondOpr = ""
-//operator = "+"
-// press equals 4 should show up! 
-// firstOpr = 3
-// secondOpr = 1 
-// operator = "+"
 
 
 
---
+++ readme.md
@@ -0,0 +1,19 @@
+#   TODO APP
+
+### Simple To Do app made with Vanilla JavaScript, HTML and CSS.
+
+![Calculator](./Calculator.png)
+
+
+## Click here for application. 
+[Calculator](https://ris345.github.io/calculator-app.github.io/)
+
+
+
+
+## Summary
+This is a calculator app made with Vanilla JS. It was a challenging project because I had to change my thinking and change my approach to a systematic way. But I loved every moment of it. 
+
+## Author
+
+### Rishav Dev Acharya - Full Stack Software Developer  [Linkedin](https://www.linkedin.com/in/rishav-acharya-0482051a7/)
